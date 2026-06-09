5fa0612e0843834018cb1857056dea29ba3b70d9
mwihaki <wikigi.git@gmail.com>
2026-06-08 20:24:12 -0400

    header with navbar and layout.js

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container flex-between">
+
+        {/* Logo */}
+        <Link href="/" className="header-logo">
+          Dishcovery
+        </Link>
+
+        {/* Navigation */}
+        <nav aria-label="Main navigation">
+          <ul className="nav-list">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/featured">Featured Dishes</Link></li>
+            <li><Link href="/recipes">Recipes</Link></li>
+            <li><Link href="/contact">Contact Us</Link></li>
+          </ul>
+        </nav>
+
+      </div>
+    </header>
+  );
+}
